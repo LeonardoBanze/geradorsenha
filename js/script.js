454b5275ade328631c882133af277e2327c092fc
Leonardo Banze <leonardobanze37@gmail.com>
2022-08-09 19:49:25 +0200

    add app

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#conatiner-password");
+
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@"
+let novaSenha = "";
+
+
+sizePassword.innerHTML = sliderElement.value
